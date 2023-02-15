8dabb1c290f992977c7b9dc127231d299a400da7
Dakedres <dakedres@debian-BULLSEYE-live-builder-AMD64>
2023-02-15 13:23:43 -0700

    Fix windows incompatability & __proto__ bug

--- src/FileIndexer.js
+++ src/FileIndexer.js
@@ -1,23 +1,23 @@
-import Fs		from 'fs/promises'
-import Path		from 'path'
-import cbor 	from 'cbor'
-import { URL }  from 'url'
-
-const basePath = Path.join(new URL('.', import.meta.url).pathname, '../')
+import Path		        from 'path'
+import Fs               from 'fs/promises'
+import { encodeAsync } 	from 'cbor'
+import { URL }          from 'url'
+import { log }          from './Logger.js'
 
 const buildDir = async path => {
-	let dir = {}
+	let dir = new Map()
 
 	for(let dirent of await Fs.readdir(path, { withFileTypes: true }) ) {
-		dir[dirent.name] = dirent.isDirectory() ? await buildDir(Path.join(path, dirent.name) ) : 0
+		dir.set(dirent.name, dirent.isDirectory() ? await buildDir(Path.join(path, dirent.name) ) : 0)
 	}
 
 	return dir
 }
 
 export const indexFiles = async subDir => {
-  let viewPath = Path.join(basePath, subDir)
-  let buf = await cbor.encodeAsync(await buildDir(viewPath ) )
+  log('updating file index')
+  let viewPath = Path.join(process.cwd(), subDir)
+  let buf = await encodeAsync(await buildDir(viewPath ) )
 
   await Fs.writeFile(Path.join(viewPath, 'files.cbor'), buf)
 }
